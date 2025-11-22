0b9730bfee796a896fff9646ac2b77b2bedc8b88
karolinesoliman-pixel <karoline.soliman@gmail.com>
2025-11-22 18:16:24 -0500

    alias resolver

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()]  [tailwindcss()],
     resolve: {
       alias: {
-        '~': isProd ? '/Software-Engineering-portfolio/my-portfolio/src' : '/my-portfolio/src'
+        '~': isProd ? 'Software-Engineering-portfolio/my-portfolio/src' : '/my-portfolio/src'
       }
     }
   }
