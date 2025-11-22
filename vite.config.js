ceb3bfc5558224168c85c5ce6ef4e687aa24845d
karolinesoliman-pixel <karoline.soliman@gmail.com>
2025-11-22 18:24:01 -0500

    full path in github context

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()]  [tailwindcss()],
     resolve: {
       alias: {
-        '~': isProd ? 'my-portfolio/src' : '/my-portfolio/src'
+        '~': isProd ? 'home/runner/work/Software-Engineering-portfolio/Software-Engineering-portfolio/my-portfolio/src' : '/my-portfolio/src'
       }
     }
   }
