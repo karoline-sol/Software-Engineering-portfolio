fb591a53cdd27d5f77a43737d48f94f338563c36
karolinesoliman-pixel <karoline.soliman@gmail.com>
2026-09-02 18:42:07 -0400

    Updates

---
+++ my-portfolio/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import "../Styles/Home.css"
+import { Link } from "react-router-dom"
+import profileimg from '../../src/assets/profileimg.png'
+export default function Home() {
+  return (
+    
+    <section className="home">
+      <div className="home-content">
+        <div className="intro">
+          <h1>
+            Hi, I’m <span className="highlight">Caroline Soliman</span>
+          </h1>
+          <h2>Front-End Developer</h2>
+          <p>
+            I'm passionate about creating efficient, user-friendly web applications 
+            that solve real-world problems. With expertise in JavaScript, React, 
+            and modern web technologies, I bring ideas to life through code.
+
+          </p>
+          <Link to="/portfolio" className="btn">
+            View My Work
+          </Link>
+          <Link to='/contact' className="btn">
+            Get In Touch
+          </Link>
+        </div>
+
+        <div className="profile-img">
+          <img src={profileimg} alt="profile-image" />
+        </div>
+      </div>
+    </section>
+    
+  )
+}
